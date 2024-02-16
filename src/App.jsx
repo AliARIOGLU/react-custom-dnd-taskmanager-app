9a3d2bad27a29e616d4a0959723f4cb293013d93
Ali <ali.arioglu06@gmail.com>
2024-02-16 21:55:40 +0300

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Board } from "./components/Board";
+
+export const App = () => {
+  return (
+    <div className="h-screen w-full bg-neutral-900 text-neutral-50">
+      <Board />
+    </div>
+  );
+};
---
+++ src/constants/column-data.js
@@ -0,0 +1,26 @@
+export const COLUMN_DATA = [
+  {
+    id: 1,
+    title: "Backlog",
+    column: "backlog",
+    headingColor: "text-neutral-500",
+  },
+  {
+    id: 2,
+    title: "TODO",
+    column: "todo",
+    headingColor: "text-yellow-200",
+  },
+  {
+    id: 3,
+    title: "In Progress",
+    column: "doing",
+    headingColor: "text-blue-200",
+  },
+  {
+    id: 4,
+    title: "Complete",
+    column: "done",
+    headingColor: "text-emerald-200",
+  },
+];
